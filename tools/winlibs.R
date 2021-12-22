ef878e9ee4f6c6663dd476cfb795a6350b2237c5
Mike Smith <grimbough@gmail.com>
2021-12-22 16:20:33 +0100

    Initial commit of Makevars.ucrt

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -4,17 +4,18 @@ DEST <- commandArgs(TRUE)[2]
 
 ## We include the trailing slash (/) so as to exclude the "ucrt"
 ## versions of the x64 binaries, which don't link properly
-ARCH <- ifelse(R.version$arch == "x86_64", "x64/", "i386/")
+ARCH <- ifelse(R.version$arch == "x86_64", "x64", "i386")
+CRT <- ifelse(R.version$crt == "ucrt", "-ucrt/", "/")
+LIB_TYPE <- paste0(ARCH, CRT)
 
 message("Copying Winlibs")
 if(getRversion() < "3.3.0") setInternet2()
-download.file(sprintf("https://github.com/rwinlib/libcurl/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+download.file(sprintf("https://github.com/rwinlib/libcurl/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE, method = "auto")
 file_path <- file.path(tempdir(), "libcurl")
 message(file_path)
 dir.create(file_path, showWarnings = TRUE)
 unzip("lib.zip", exdir = file_path)
-list.files(file_path, recursive = TRUE, full.names = TRUE)
-arch_files <- grep(ARCH, x = list.files(file_path, recursive = TRUE, full.names = TRUE), value = TRUE)
+arch_files <- grep(LIB_TYPE, x = list.files(file_path, recursive = TRUE, full.names = TRUE), value = TRUE)
 
 message(arch_files)
 message("ARCH: ", ARCH)

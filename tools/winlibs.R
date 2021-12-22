61ddcabf1fceb9b6f55ae0027f476c0d7396673f
Mike Smith <grimbough@gmail.com>
2021-12-22 16:43:43 +0100

    Work in progress

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,6 +1,7 @@
 # Build against static libraries from rwinlib
 VERSION <- commandArgs(TRUE)[1]
 DEST <- commandArgs(TRUE)[2]
+INCLUDE_DEST <- commandArgs(TRUE)[3]
 
 ## We include the trailing slash (/) so as to exclude the "ucrt"
 ## versions of the x64 binaries, which don't link properly
@@ -17,8 +18,14 @@ dir.create(file_path, showWarnings = TRUE)
 unzip("lib.zip", exdir = file_path)
 arch_files <- grep(LIB_TYPE, x = list.files(file_path, recursive = TRUE, full.names = TRUE), value = TRUE)
 
-message(arch_files)
 message("ARCH: ", ARCH)
 
-lapply(arch_files, file.copy, to = DEST)
+curl_headers <- grep("\\.h$", x = list.files(file_path, recursive = TRUE, full.names = TRUE), value = TRUE)
+
+message("Copying libraries")
+file.copy(arch_files,  to = DEST)
+
+message("Copying headers")
+file.copy(curl_headers, to = INCLUDE_DEST)
+
 unlink("lib.zip")

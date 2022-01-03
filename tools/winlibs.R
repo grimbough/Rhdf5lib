0100ea757dcbb366107f8fb29d4d7e184d88ec59
Mike Smith <grimbough@gmail.com>
2022-01-03 11:49:40 +0100

    Copying pre-built windows libraries

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -9,21 +9,15 @@ ARCH <- ifelse(R.version$arch == "x86_64", "x64", "i386")
 CRT <- ifelse(R.version$crt == "ucrt", "-ucrt/", "/")
 LIB_TYPE <- paste0(ARCH, CRT)
 
-message("Copying Winlibs")
+message("Downloading dependencies")
 if(getRversion() < "3.3.0") setInternet2()
 download.file(sprintf("https://github.com/rwinlib/libcurl/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE, method = "auto")
 file_path <- file.path(tempdir(), "libcurl")
-message(file_path)
 dir.create(file_path, showWarnings = TRUE)
 unzip("lib.zip", exdir = file_path)
 arch_files <- grep(LIB_TYPE, x = list.files(file_path, recursive = TRUE, full.names = TRUE), value = TRUE)
 
-message("LIB TYPE: ", LIB_TYPE)
-
 message("Copying libraries")
-message(arch_files)
-message(DEST)
-file.copy(arch_files,  to = DEST)
+invisible(file.copy(arch_files,  to = DEST))
 
-
-unlink("lib.zip")
+file.remove("lib.zip")

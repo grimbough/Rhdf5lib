c4d5cc57f9fc00664c8b62e2fac5beab1f6a5984
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-05-01 10:22:50 -0700

    Restored SZIP support.

--- tools/libaec_setup.R
+++ tools/libaec_setup.R
@@ -4,38 +4,28 @@ options <- biocmake::formatArguments(biocmake::configure(fortran.compiler=FALSE)
 aec.tgz <- "libaec-source.tar.gz"
 
 options <- c(options, 
-    "-DBUILD_SHARED_LIBS=OFF",
-    "-DHDF5_BUILD_CPP_LIB=ON",
     "-DBUILD_TESTING=OFF",
-    "-DHDF5_BUILD_TOOLS=OFF",
-    "-DHDF5_BUILD_EXAMPLES=OFF",
-    "-DHDF5_BUILD_UTILS=OFF",
-    paste0("-DCMAKE_PREFIX_PATH=", file.path(getwd(), "_temp_libaec", "install")),
-    "-DHDF5_USE_LIBAEC_STATIC=ON",
-    #"-DHDF5_ENABLE_PLUGIN_SUPPORT=ON",
-    #"-DHDF5_ENABLE_HDFS=ON",
-    #"-DHDF5_MINGW_STATIC_GCC_LIBS=ON",
     NULL
 )
 
-install_path <- file.path("inst", "hdf5")
+tmp_dir <- "_temp_libaec"
+install_path <- file.path(tmp_dir, "install")
 
 if (!file.exists(install_path)) {
-    tmp_dir <- "_temp_hdf5"
     dir.create(tmp_dir, recursive=TRUE, showWarnings=FALSE)
     build_path <- file.path(tmp_dir, "build")
 
     if (!file.exists(build_path)) {
         source_path <- file.path(tmp_dir, "source")
         if (!file.exists(source_path)) {
-            stopifnot(untar("hdf5-source.tar.gz", exdir=tmp_dir) == 0)
-            first <- list.files(tmp_dir, pattern="^hdf5-")
+            stopifnot(untar("libaec-source.tar.gz", exdir=tmp_dir) == 0)
+            first <- list.files(tmp_dir, pattern="^libaec-")
             file.rename(file.path(tmp_dir, first), source_path)
         }
 
         options <- c(options, paste0("-DCMAKE_INSTALL_PREFIX=", install_path))
         if (system2(cmake, c("-S", source_path, "-B", build_path, options), stderr=FALSE) != 0) {
-            stop("failed to configure the HDF5 library with CMake")
+            stop("failed to configure the libaec library with CMake")
         }
     }
 
@@ -45,12 +35,12 @@ if (!file.exists(install_path)) {
         status <- system2(cmake, c("--build", build_path, "--config", "Release"))
     }
     if (status != 0) {
-        stop("failed to build the HDF5 library with CMake")
+        stop("failed to build the libaec library with CMake")
     }
 
     dir.create("inst", showWarnings=FALSE)
     status <- system2(cmake, c("--install", build_path), stderr=FALSE)
     if (status != 0) {
-        stop("failed to install the HDF5 library with CMake")
+        stop("failed to install the libaec library with CMake")
     }
 }

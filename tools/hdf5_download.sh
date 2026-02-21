bafc7d8c9feea49dd5975451f6c6ade3f2f1d960
Hugo Gruson <git@hugogruson.fr>
2026-02-21 11:11:46 +0100

    Add comment about strip-components

--- tools/hdf5_download.sh
+++ tools/hdf5_download.sh
@@ -3,6 +3,8 @@
 curl -L https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.7/src/hdf5-1.10.7.tar.bz2 -o hdf5.tar.bz2
 rm -rf vendor/hdf5
 mkdir -p vendor/hdf5
+# strip-components is "dangerous" because it we go too deep, we mess up the entire folder structure.
+# If the compilation suddenly fails for apparently no reason, check this.
 tar -xf hdf5.tar.bz2 -C vendor/hdf5 --strip-components=1
 rm hdf5.tar.bz2
 
